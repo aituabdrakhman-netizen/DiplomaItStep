2e1887e0f3a43c4c95a169abe23fe5c7f55691dc
ITSTART <itstart@step.kz>
2026-06-24 14:38:16 +0500

    ввод и история весов и целей

--- main.py
+++ main.py
@@ -38,3 +38,35 @@ def show_workouts(data):
         for i in range(len(data["workouts"])):
             workout = data["workouts"][i]
             print(i + 1, "-", workout["date"], "-", workout["exercise"])
+
+def add_weight(data):
+    try:
+        weight = float(input("Введите вес: "))
+        data["weights"].append(weight)
+        save_data(data)
+        print("Вес сохранён.")
+    except:
+        print("Ошибка ввода!")
+
+
+def show_weights(data):
+    if len(data["weights"]) == 0:
+        print("История веса пуста.")
+    else:
+        print("\n=== ИСТОРИЯ ВЕСА ===")
+        for weight in data["weights"]:
+            print(weight, "кг")
+
+
+def set_goal(data):
+    goal = input("Введите цель: ")
+    data["goal"] = goal
+    save_data(data)
+    print("Цель сохранена.")
+
+
+def show_goal(data):
+    if data["goal"] == "":
+        print("Цель не установлена.")
+    else:
+        print("Текущая цель:", data["goal"])
